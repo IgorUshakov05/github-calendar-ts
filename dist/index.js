c65028e1b7e6b8a2e4e1fb5f46bf8e70d2a5a1bd
Igor <igorushakov111@gmail.com>
2025-03-09 11:43:00 +0700

    Calendar

--- dist/index.js
+++ dist/index.js
@@ -7,13 +7,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import dotenv from "dotenv";
-dotenv.config();
 export function GetEventData(_a) {
     return __awaiter(this, arguments, void 0, function* ({ username, token, }) {
         const perPage = 100;
         const page = 1;
-        const apiUrl = `https://api.github.com/users/${username}/events/public?per_page=${perPage}&page=${page}`;
+        const apiUrl = `https://api.github.com/users/${username}/events?per_page=${perPage}&page=${page}`;
         try {
             const response = yield fetch(apiUrl, {
                 headers: {
@@ -38,8 +36,53 @@ export function GetEventData(_a) {
 const getEvent = () => __awaiter(void 0, void 0, void 0, function* () {
     let data = yield GetEventData({
         username: "IgorUshakov05",
-        token: process.env.GITHUB_TOKEN || "",
+        token: "",
     });
     console.log(data.events);
+    const months = {
+        1: "Jan",
+        2: "Feb",
+        3: "Mar",
+        4: "Apr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Aug",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dec",
+    };
+    const monthContainer = document.querySelector(".month");
+    const grid = document.querySelector(".grid");
+    for (let item = 1; item < 13; item++) {
+        const span = document.createElement("span");
+        span.innerText = months[item];
+        span.className = "monthItem";
+        monthContainer === null || monthContainer === void 0 ? void 0 : monthContainer.appendChild(span);
+    }
+    const startDate = new Date(new Date().getFullYear(), 0, 1);
+    const totalWeeks = 53;
+    const daysPerWeek = 7;
+    for (let week = 0; week < totalWeeks; week++) {
+        const weekContainer = document.createElement("div");
+        weekContainer.classList.add("week");
+        for (let dayOfWeek = 0; dayOfWeek < daysPerWeek; dayOfWeek++) {
+            const day = document.createElement("div");
+            day.classList.add("day");
+            const date = new Date(startDate);
+            date.setDate(startDate.getDate() + week * 7 + dayOfWeek - startDate.getDay() + 1);
+            const attr = date.toISOString().split("T")[0];
+            const today = new Date().toISOString().split("T")[0];
+            const dayOfMonth = date.getDate();
+            if (attr.includes(`${new Date().getFullYear() - 1}`) ||
+                attr.includes(`${new Date().getFullYear() + 1}`)) {
+                day.style.opacity = "0";
+            }
+            day.setAttribute("data-date", attr);
+            weekContainer.appendChild(day);
+        }
+        grid === null || grid === void 0 ? void 0 : grid.appendChild(weekContainer);
+    }
 });
 getEvent();
